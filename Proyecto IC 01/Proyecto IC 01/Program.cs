9dd08932c1a04b8863b0f66039ab12d3df89c199
José Dissér Lulimache Maldonado <josedissermaldonado@gmail.com>
2024-11-14 12:38:16 -0500

    Mi primera clase

--- Proyecto IC 01/Proyecto IC 01/Program.cs
+++ Proyecto IC 01/Proyecto IC 01/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Mi nombre es José Dissér");
+Console.WriteLine("Mi Clase es GIT");
